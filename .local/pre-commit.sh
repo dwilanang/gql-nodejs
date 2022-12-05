4b000d8d60cc54d4527e16fcd9dc924f3701a8ff
Dwi Lanang <dwilanang@192.168.1.5>
2022-12-06 01:39:02 +0700

    test

--- .local/pre-commit.sh
+++ .local/pre-commit.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env sh
 
 if [ -n "$SKIP_PRE_COMMIT" ]; then
-  echo "✔️ Skipping pre-commit because env var SKIP_PRE_COMMIT exists and not-empty"
+  echo "Skipping pre-commit"
   exit 0
 fi
 
-CHANGED_FILES=$(git diff HEAD --name-only | egrep '\.js$')
-
-if [ -z "$CHANGED_JS_FILES" ]; then
-
-  echo "✌️ No files changed ✌️"
-
-else
-  echo "🔎 Linting"
+files=$(git diff --cached --name-only --diff-filter=ACM | grep "\.(js|yml|graphql|json)$")
+if [ "$files" != "" ]; then
   if ! make lint; then
-    echo "⛔ Code not clean, linting failed"
+      echo "Code not clean, linting failed"
     exit 1
   fi
-  echo "✔️ Lint OK"
-
 fi
 
-echo "✔️ Pre-Commit OK"
+echo "✔️"
 exit 0
--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
+init:
+	@.local/init.sh
+
 lint:
-	@which eslint 2>/dev/null || echo "please install eslint : npm install eslint --save-dev"
+	@which eslint 2> /dev/null || echo "please install eslint : npm install eslint --save-dev"
 	@npm run lint
-	
+
 service-up:
-	@echo "Starting the service - $$(date)"
 	@docker-compose -f docker-compose.yml up --build
+
 service-down:
 	@docker-compose -f docker-compose.yml down
 
